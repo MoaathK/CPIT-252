99d6e6b4e16a3a4fc5d9e43b944dbab1a4a796e2
Moath Alahmadi <MoaathK@users.noreply.github.com>
2024-02-01 10:34:48 +0300

    -

--- Ex-2/Ex-2/courseinfo/CourseInformation.java
+++ Ex-2/Ex-2/courseinfo/CourseInformation.java
@@ -1,8 +1,6 @@
 package courseinfo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class CourseInformation {
     public static void main(String[] args) throws IOException{
