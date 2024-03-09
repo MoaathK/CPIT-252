e72a07d391bea3ec2f097af1dd2334ced207e097
Moath <109114154+MoaathK@users.noreply.github.com>
2024-03-10 01:16:26 +0300

    bug fixing

--- LabExample/Ex-6/shape/PrototypePatternDemo.java
+++ LabExample/Ex-6/shape/PrototypePatternDemo.java
@@ -7,13 +7,13 @@ public class PrototypePatternDemo {
         ShapeCache.loadCache();
 
         Shape clonedShape = (Shape) ShapeCache.getShape("1");
-        System.out.println("facdePatternDemo.Shape: "+ clonedShape.getType());
+        System.out.println("Shape: "+ clonedShape.getType());
 
         Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
-        System.out.println("facdePatternDemo.Shape: "+ clonedShape2.getType());
+        System.out.println("Shape: "+ clonedShape2.getType());
 
         Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
-        System.out.println("facdePatternDemo.Shape: "+ clonedShape3.getType());
+        System.out.println("Shape: "+ clonedShape3.getType());
 
 
         //later can do clone factory for practises
