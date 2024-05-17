70122b2092a4dda57863352b4e257eb6eacfaa59
MoaathK <moathkalahmadi@outlook.com>
2024-05-17 15:51:28 +0300

    performing some testing on methods

--- LabExample/StrategyPattern/Test/Main.java
+++ LabExample/StrategyPattern/Test/Main.java
@@ -1,7 +1,20 @@
 package Test;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        Scanner input = new Scanner(System.in);
+        String[] listOfNames = new String[3];
+        for(int i = 0; i < 3; i++) {
+            System.out.print("Enter your name: ");
+            String name = input.nextLine();
+            listOfNames[i] = name;
+
+        }
+        System.out.println("This is all the names: ");
+        for(int i = 0; i < listOfNames.length; i++) {
+            System.out.println(listOfNames[i]);
+        }
     }
 }
