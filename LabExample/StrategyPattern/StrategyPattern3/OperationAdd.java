c7bc8a3b12257d68f879ef152d156acf88f1a8b3
MoaathK <moathkalahmadi@outlook.com>
2024-05-17 17:46:27 +0300

    Strategy pattern examples

--- LabExample/StrategyPattern/StrategyPattern3/OperationAdd.java
+++ LabExample/StrategyPattern/StrategyPattern3/OperationAdd.java
@@ -3,7 +3,7 @@ package StrategyPattern3;
 public class OperationAdd implements Strategy{
     @Override
     public int doOperation(int num1, int num2) {
-        return num1+num1;
+        return num1+num2;
     }
 
 }
